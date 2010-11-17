3f598d7a1d7e846ad727e7232cb90b09f726dda5
Christian Eichhorn <c.eichhorn@webmasters.de>
2010-11-17 16:10:08 +0100

    added Mysql2 adapter
    Conflicts:
    
    	lib/rails_sql_views/loader.rb

--- lib/rails_sql_views/loader.rb
+++ lib/rails_sql_views/loader.rb
@@ -1,7 +1,7 @@
 
 module RailsSqlViews
   module Loader
-    SUPPORTED_ADAPTERS = %w( Mysql PostgreSQL SQLServer SQLite SQLite3 OracleEnhanced )
+    SUPPORTED_ADAPTERS = %w( Mysql Mysql2 PostgreSQL SQLServer SQLite SQLite3 OracleEnhanced )
 
     def self.load_extensions
       SUPPORTED_ADAPTERS.each do |db|
